68ef786e1ff18e4d42b5be21370269a10ae147f7
Алексей Челышев <alexey.chelyshev.pvm@gmail.com>
2017-10-23 19:23:00 +0300

    панель действий

--- app/src/main/java/bitsandpizzas/hfad/com/bitsandpizzas/MainActivity.java
+++ app/src/main/java/bitsandpizzas/hfad/com/bitsandpizzas/MainActivity.java
@@ -1,13 +1,55 @@
 package bitsandpizzas.hfad.com.bitsandpizzas;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ShareActionProvider;
+
 
 public class MainActivity extends Activity {
+
+    private ShareActionProvider shareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Заполнение меню; элементы (если они есть) добавляются на панель действий.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        shareActionProvider = (ShareActionProvider) menuItem.getActionProvider();
+        setIntent("This is example text");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    private void setIntent(String text) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        shareActionProvider.setShareIntent(intent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_create_order:
+                //Код, выполняемый при выборе элемента Create rOder
+                Intent intent = new Intent(this, OrderActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_settings:
+                //Код, выполняемый при выборе элемента Settings
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
